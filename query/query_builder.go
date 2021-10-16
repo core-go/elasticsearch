e8695396f33492a601ad915a0d68baa82355d999
Duc Nguyen <minhduc140583@gmail.com>
2021-10-16 18:53:15 +0700

    Rename SearchModel to Filter

--- query/query_builder.go
+++ query/query_builder.go
@@ -20,21 +20,20 @@ func (b *Builder) BuildQuery(sm interface{}) map[string]interface{} {
 
 func Build(sm interface{}, resultModelType reflect.Type) map[string]interface{} {
 	query := map[string]interface{}{}
-	if _, ok := sm.(*search.SearchModel); ok {
+	if _, ok := sm.(*search.Filter); ok {
 		return query
 	}
 	value := reflect.Indirect(reflect.ValueOf(sm))
 	numField := value.NumField()
 	for i := 0; i < numField; i++ {
 		fieldValue := value.Field(i).Interface()
-		if v, ok := fieldValue.(*search.SearchModel); ok {
-			if len(v.Excluding) > 0 {
-				for key, val := range v.Excluding {
-					if len(val) > 0 {
-						actionDateQuery := map[string]interface{}{}
-						actionDateQuery["$nin"] = val
-						query[key] = actionDateQuery
-					}
+		if v, ok := fieldValue.(*search.Filter); ok {
+			if v.Excluding != nil && len(v.Excluding) > 0 {
+				_, _, columnName := getFieldByBson(value.Type(), "_id")
+				if len(columnName) > 0 {
+					actionDateQuery := map[string]interface{}{}
+					actionDateQuery["$nin"] = v.Excluding
+					query[columnName] = actionDateQuery
 				}
 			}
 			continue
@@ -43,22 +42,22 @@ func Build(sm interface{}, resultModelType reflect.Type) map[string]interface{} 
 
 			actionDateQuery := map[string]interface{}{}
 
-			actionDateQuery["$gte"] = rangeDate.StartDate
+			actionDateQuery["$gte"] = rangeDate.Min
 			query[columnName] = actionDateQuery
-			var eDate = rangeDate.EndDate.Add(time.Hour * 24)
-			rangeDate.EndDate = &eDate
-			actionDateQuery["$lte"] = rangeDate.EndDate
+			var eDate = rangeDate.Max.Add(time.Hour * 24)
+			rangeDate.Max = &eDate
+			actionDateQuery["$lte"] = rangeDate.Max
 			query[columnName] = actionDateQuery
 		} else if rangeDate, ok := fieldValue.(*search.DateRange); ok && rangeDate != nil {
 			_, columnName := findFieldByName(resultModelType, value.Type().Field(i).Name)
 
 			actionDateQuery := map[string]interface{}{}
 
-			actionDateQuery["$gte"] = rangeDate.StartDate
+			actionDateQuery["$gte"] = rangeDate.Min
 			query[columnName] = actionDateQuery
-			var eDate = rangeDate.EndDate.Add(time.Hour * 24)
-			rangeDate.EndDate = &eDate
-			actionDateQuery["$lte"] = rangeDate.EndDate
+			var eDate = rangeDate.Max.Add(time.Hour * 24)
+			rangeDate.Max = &eDate
+			actionDateQuery["$lte"] = rangeDate.Max
 			query[columnName] = actionDateQuery
 		} else if rangeTime, ok := fieldValue.(search.TimeRange); ok {
 			_, columnName := findFieldByName(resultModelType, value.Type().Field(i).Name)
@@ -121,7 +120,7 @@ func Build(sm interface{}, resultModelType reflect.Type) map[string]interface{} 
 			query[columnName] = actionDateQuery
 		} else {
 			t := value.Field(i).Kind().String()
-			if _, ok := fieldValue.(*search.SearchModel); t == "bool" || (strings.Contains(t, "int") && fieldValue != 0) || (strings.Contains(t, "float") && fieldValue != 0) || (!ok && t == "string" && value.Field(i).Len() > 0) || (!ok && t == "ptr" &&
+			if _, ok := fieldValue.(*search.Filter); t == "bool" || (strings.Contains(t, "int") && fieldValue != 0) || (strings.Contains(t, "float") && fieldValue != 0) || (!ok && t == "string" && value.Field(i).Len() > 0) || (!ok && t == "ptr" &&
 				value.Field(i).Pointer() != 0) {
 				_, columnName := findFieldByName(resultModelType, value.Type().Field(i).Name)
 				if len(columnName) > 0 {
@@ -146,4 +145,19 @@ func findFieldByName(modelType reflect.Type, fieldName string) (index int, jsonT
 		}
 	}
 	return -1, fieldName
+}
+func getFieldByBson(modelType reflect.Type, bsonName string) (int, string, string) {
+	numField := modelType.NumField()
+	for i := 0; i < numField; i++ {
+		field := modelType.Field(i)
+		tag1, ok1 := field.Tag.Lookup("bson")
+		if ok1 && strings.Split(tag1, ",")[0] == bsonName {
+			if tag2, ok2 := field.Tag.Lookup("json"); ok2 {
+				json := strings.Split(tag2, ",")[0]
+				return i, field.Name, json
+			}
+			return i, field.Name, ""
+		}
+	}
+	return -1, bsonName, bsonName
 }
